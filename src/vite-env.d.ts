b776204b3730c4ca1b3d74f7afce2993fa4c4573
Ameer Rahman <ameerrahman456@gmail.com>
2026-03-06 00:55:48 -0500

    fixed missing css imports for vercel deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,11 @@
-
+/// <reference types="vite/client" />
 
 declare module "*.json" {
   const value: any;
   export default value;
+}
+
+declare module "*.css" {
+  const styles: { [className: string]: string };
+  export default styles;
 }
